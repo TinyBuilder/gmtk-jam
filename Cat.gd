f124ddc848c41ff4f2c010d7d9bc63ee65526071
ivan <ivan.fernando.w@gmail.com>
2020-07-11 13:13:57 +0000

    Implemented walls and finish line and cucumbers

--- Cat.gd
+++ Cat.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends KinematicBody2D
 
 
 # Declare member variables here. Examples:
@@ -8,62 +8,81 @@ var velocity = Vector2()
 var screen_size
 var rng
 var targets = []
+var threats = []
+var is_chasing = false
+var is_eating = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
 
 func start(random):
-	print(self.name)
 	rng = random
 	show()
-	self.idle()
+	idle()
 
 func idle():
+	is_chasing = false
+	velocity.x = 0
+	velocity.y = 0
 	$Timer.start(rng.randf_range(0.0, MAX_WAIT))
 
-class TargetSorter:
-	func prioritise_target(a, b):
-		var d1 = a.global_position.distance_to(self.global_position)
-		var d2 = b.global_position.distance_to(self.global_position)
-		if d1 < d2:
-			 return true
-		return false
+func prioritise_target(a, b):
+	var d1 = a.global_position.distance_to(global_position)
+	var d2 = b.global_position.distance_to(global_position)
+	if d1 < d2:
+		 return true
+	return false
 
 func add_target(target):
 	targets.append(target)
 
-func remove_target(target):
-	targets.erase(target)
+func remove_target(target, eater):
+	if is_chasing and targets.size() > 0 and target == targets[0]:
+		targets.erase(target)
+		if eater == self:
+			idle()
+		else:
+			evaluate()
+	else:
+		targets.erase(target)
+
+
+func add_threat(threat):
+	if threat.global_position.distance_to(global_position) < 50 and rng.randf_range(0.0, 10.0) < 7.5:
+		var speed = rng.randf_range(1.5, 2.0) * MAX_SPEED
+		velocity.x = global_position.x - threat.global_position.x
+		velocity.y = global_position.y - threat.global_position.y
+		velocity = velocity.normalized() * speed
+		$Timer.start(rng.randf_range(0.5, 1.0))
 
 func evaluate():
 	if rng.randf_range(0.0, 10.0) < 9.0:
 		var speed = rng.randf() * MAX_SPEED
 		
 		if targets.size() > 0 and rng.randf_range(0.0, 10.0) < 9.0:
-			targets.sort_custom(TargetSorter, "prioritise_target")
-			velocity.x = targets[0].global_position.x - self.position.x
-			velocity.y = targets[0].global_position.y - self.position.y
+			is_chasing = true
+			targets.sort_custom(self, "prioritise_target")
+			velocity.x = targets[0].global_position.x - global_position.x
+			velocity.y = targets[0].global_position.y - global_position.y
 		else:
+			is_chasing = false
 			velocity.x = rng.randf_range(-1.0, 1.0)
 			velocity.y = rng.randf_range(-1.0, 1.0)
 		
 		velocity = velocity.normalized() * speed
 		$Timer.start(rng.randf_range(0.0, MAX_WAIT))
-		if velocity.x > 0:
-			$AnimatedSprite.set_flip_h(false)
-		else:
-			$AnimatedSprite.set_flip_h(true)	
-		$AnimatedSprite.play("walk")
+		
 	else:
-		velocity.x = 0
-		velocity.y = 0
-		self.idle()
+		idle()
 
 func _on_Timer_timeout():
-	self.evaluate()
+	evaluate()
 
 func _process(delta):
-	self.position += velocity * delta
-	self.position.x = clamp(self.position.x, 0, screen_size.x)
-	self.position.y = clamp(self.position.y, 0, screen_size.y)
+	if velocity.x > 0:
+		$AnimatedSprite.set_flip_h(false)
+	elif velocity.x < 0:
+		$AnimatedSprite.set_flip_h(true)	
+	$AnimatedSprite.play("walk")
+	move_and_slide(velocity)
--- Game.gd
+++ Game.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 export (PackedScene) var Fish
 export (PackedScene) var Cat
+export (PackedScene) var Cucumber
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -30,11 +31,25 @@ func _ready():
 
 func _input(event):
    # Mouse in viewport coordinates
-	if event is InputEventMouseButton and event.get_button_index() == 1 and event.is_pressed() :
-		var fish = Fish.instance()
-		add_child(fish)
-		fish.position = event.position
-		for i in range(MAX_CATS):
-			fish.connect("fish_spawn", cats[i], "add_target")
-			fish.connect("fish_eaten", cats[i], "remove_target")
-		fish.spawn()
+	if event is InputEventMouseButton and event.is_pressed() :
+		if event.get_button_index() == 1:
+			var fish = Fish.instance()
+			add_child(fish)
+			fish.position = event.position
+			for i in range(MAX_CATS):
+				fish.connect("fish_spawn", cats[i], "add_target")
+				fish.connect("fish_eaten", cats[i], "remove_target")
+			fish.spawn()
+		elif event.get_button_index() == 2:
+			var cucumber = Cucumber.instance()
+			add_child(cucumber)
+			cucumber.position = event.position
+			for i in range(MAX_CATS):
+				cucumber.connect("cucumber_spawn", cats[i], "add_threat")
+			cucumber.spawn() 
+
+
+func _on_Area2D_body_entered(body):
+	if body.name.match("**Cat****"):
+		print(body.name + " wins!")
+
